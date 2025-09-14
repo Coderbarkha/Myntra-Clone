534ac7f044f403c41292648cdc01a2fa54ca0de5
Coderbarkha <barkhathakkar241@gmail.com>
2025-09-14 22:31:27 +0530

    Add myntra -hero image and enhance the placement of banner image ,add both images into Home.jsx and enhance styling of images in global.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,31 @@
-import React from'react';
-import Products from "./Products";
+import React from "react";
 import banner from "../assets/myntra-banner.PNG";
-const Home =()=>{
-    return(
-        <div className='home-container'>
-            <img src={banner} className="w-full h-[400px] object-cover" alt='myntra banner'/>
-        </div>
-    );
-}
+import myntrahero from "../assets/myntra-hero2.PNG";
+import sliderimage1 from "../assets/sliderimg1.PNG";
+import "../styles/global.css";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="banner-container">
+        <img
+          src={banner}
+          className="w-full h-[400px] object-cover myntra-banner"
+          alt="myntra banner"
+        />
+        <img
+          src={myntrahero}
+          className="w-full h-[400px] object-cover myntra-banner"
+          alt="myntra hero"
+        />
+        <img
+          src={sliderimage1}
+          className="w-full h-[400px] object-cover myntra-banner"
+          alt="slider image 1"
+        />
+
+      </div>
+    </div>
+  );
+};
 
 export default Home;
